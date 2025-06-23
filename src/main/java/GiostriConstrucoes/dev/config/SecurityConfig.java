f506a293623f27fe15d7845f6d4ccb30745c007a
Leandro Alencar <leandro130333.dev@gmail.com>
2025-06-22 23:07:00 -0300

    Habilitando o vercel

--- src/main/java/GiostriConstrucoes/dev/config/SecurityConfig.java
+++ src/main/java/GiostriConstrucoes/dev/config/SecurityConfig.java
@@ -63,6 +63,7 @@ public class SecurityConfig implements WebMvcConfigurer {
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://127.0.0.1:5500",
                 "http://localhost:5500",
+                "https://giostri-front.vercel.app",
                 "null"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
